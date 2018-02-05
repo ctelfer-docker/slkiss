6e3835f7ae8bf411c387a5c931e97279f72f4744
Chris Telfer <ctelfer@docker.com>
2018-02-05 15:17:15 -0500

    Rename env variables and removed baked-in ones

--- issuebot.go
+++ issuebot.go
@@ -7,40 +7,63 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/ctelfer-docker/slkiss/slack"
 )
 
-var repo  = flag.String("r", "ctelfer-docker/slkiss", "Default repository to manage")
-var user  = flag.String("u", "ctelfer-docker", "Github user for the bot to operate as")
-var auth  = flag.String("a", "", "Authentication token -- alternately set GHMOD_PASSWORD")
+var repo  = flag.String("r", "", "Default repository to manage")
+var user  = flag.String("u", "", "Github user for the bot to operate as")
+var auth  = flag.String("a", "", "Authentication token")
 var addr  = flag.String("l", "", "Address to listen on")
 var port  = flag.Uint("p", 80, "Port to listen on")
-var cfgfn = flag.String("c", "", "Config field to load")
 
-const authEnv = "GHMOD_PASSWORD"
+const (
+	repoEnv = "ISSUEBOT_REPO"
+	userEnv = "ISSUEBOT_USER"
+	authEnv = "ISSUEBOT_AUTH"
+	addrEnv = "ISSUEBOT_LADDR"
+	portEnv = "ISSUEBOT_LPORT"
+)
 
 func main() {
+	getEnv()
 	flag.Parse()
-	if *auth == "" {
-		s, ok := os.LookupEnv(authEnv)
-		if !ok {
-			usage()
-		}
-		*auth = s
+	if *repo == "" || *user == "" || *auth == "" {
+		usage()
 	}
 	astr := fmt.Sprintf("%s:%d", *addr, *port)
 	bot := slack.NewIssueBot(astr, *repo)
 	bot.SetGithubAuth(encodeBasicAuth(*user, *auth))
-	loadConfig(bot)
 	log.Println("Starting bot")
 	bot.Run()
 }
 
+func getEnv() {
+	if s, ok := os.LookupEnv(repoEnv); ok { *repo = s }
+	if s, ok := os.LookupEnv(userEnv); ok { *user = s }
+	if s, ok := os.LookupEnv(authEnv); ok { *auth = s }
+	if s, ok := os.LookupEnv(addrEnv); ok { *addr = s }
+	if s, ok := os.LookupEnv(portEnv); ok {
+		p, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatal("Error with local port:", err)
+		}
+		*port = uint(p)
+	}
+}
+
 func usage() {
 	fmt.Fprintf(os.Stderr, "usage: %s\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "\t* Authentication token is required unless set in GHMOD_PASSWORD\n")
 	flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "-----\n")
+	fmt.Fprintf(os.Stderr, "\t* User, Repo and Authentication token are all required\n")
+	fmt.Fprintf(os.Stderr, "\t* One can also set these options via environment variables:\n")
+	fmt.Fprintf(os.Stderr, "\t*   %s - repository\n", repoEnv)
+	fmt.Fprintf(os.Stderr, "\t*   %s - github user\n", userEnv)
+	fmt.Fprintf(os.Stderr, "\t*   %s - github authentication password\n", authEnv)
+	fmt.Fprintf(os.Stderr, "\t*   %s - local address\n", addrEnv)
+	fmt.Fprintf(os.Stderr, "\t*   %s - local port\n", portEnv)
 	os.Exit(1)
 }
 
@@ -48,12 +71,4 @@ func encodeBasicAuth(u string, pw string) string {
 	s := u + ":" + pw
 	b64 := base64.StdEncoding.EncodeToString([]byte(s))
 	return "Basic " + b64
-}
-
-func loadConfig(bot *slack.IssueBot) {
-	if *cfgfn == "" {
-		return
-	}
-	log.Println("Loading config file")
-	// TODO
 }
